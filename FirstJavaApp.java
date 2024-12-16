90b9534a994f711b9898b6880fb376fa3a70e9e2
botniedata <botniedata@gmail.com>
2024-12-16 09:48:01 +0800

    adds FirstJavaApp file

---
+++ FirstJavaApp.java
@@ -0,0 +1,11 @@
+// class: a container for organizing Java code
+// comment: human readable information in Java code that Java ignore
+class Main { 
+    
+    // method: like sub-container for code inside tof classes
+    public static void main(String[] args) {
+    
+        // Print line
+        System.out.println("Welcome to Java!");
+    }
+}
