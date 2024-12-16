8d286502d86f87767371161debba2e8f69848a4a
botniedata <botniedata@gmail.com>
2024-12-16 10:00:32 +0800

    update the commenting on app

--- FirstJavaApp.java
+++ FirstJavaApp.java
@@ -1,11 +1,19 @@
+/*
+ * Multi-line comments are accomplished with forward slash and asterisk and then asterisk slash
+ */
+
 // class: a container for organizing Java code
 // comment: human readable information in Java code that Java ignore
+// Class Starts
 class Main { 
     
     // method: like sub-container for code inside tof classes
+    // main method Starts
     public static void main(String[] args) {
     
-        // Print line
+        // Print out statement
         System.out.println("Welcome to Java!");
+    // main method Ends
     }
+// Class Ends
 }
