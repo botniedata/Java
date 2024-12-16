a58bc680e6499833bde511e729bfcf93362ab899
botniedata <botniedata@gmail.com>
2024-12-16 13:54:36 +0800

    update PickType java file

---
+++ PickType.java
@@ -0,0 +1,26 @@
+class PickType {
+    public static void main (String[] args) {
+        // Assign Variables
+        char tshirtSize = 'M';          // alphabet, punctuations, numbers, etc.
+        byte dressSize = 8;             // -128 to 128
+        boolean is0n = false;           // true or false
+        float interestRate = 5.45f;     // 3.4e-038 to 3.4e+038
+        // Print line 
+        System.out.println(tshirtSize);
+        System.out.println(dressSize);
+        System.out.println(is0n);
+        System.out.println(interestRate);
+
+        // Assign Variables
+        long nationalDebt = 34577128968551L;    // integers between -2^63 and 2^63 (use 64 bits of memory)
+        float temp = 98.6F;                     // 3.4e-038 to 3.4e+038
+        double pi = 3.515926535;                // 1.7e-308 tp 1.8e+308
+        boolean isSmoker = false;               // true or false
+
+        // Print line 
+        System.out.println(nationalDebt);
+        System.out.println(temp);
+        System.out.println(pi);
+        System.out.println(isSmoker);
+    }
+}
